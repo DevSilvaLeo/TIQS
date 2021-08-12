07abf8b8a871791634477e4656801f5c404679ce
AndrePoubel <75315688+AndrePoubel@users.noreply.github.com>
2021-08-12 19:03:57 -0300

    linde-digital
    arquivos

---
+++ Linde/src/webparts/whoWeAre/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IWhoWeAreWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'WhoWeAreWebPartStrings' {
+  const strings: IWhoWeAreWebPartStrings;
+  export = strings;
+}
